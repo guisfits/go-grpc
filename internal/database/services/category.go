ed8859bf11bb750493418a61591ebd6d1ab8ab51
Guilherme Camargo <guisfits@gmail.com>
2023-03-07 22:15:09 -0300

    category service

---
+++ internal/database/services/category.go
@@ -0,0 +1,32 @@
+package service
+
+import (
+	"context"
+
+	"github.com/guisfits/fullcycle-go-grpc/internal/database"
+	"github.com/guisfits/fullcycle-go-grpc/internal/pb"
+)
+
+type CategoryService struct {
+	pb.UnimplementedCategoryServiceServer
+	CategoryDB database.Category
+}
+
+func NewCategoryService(categoryDB database.Category) *CategoryService {
+	return &CategoryService{CategoryDB: categoryDB}
+}
+
+func (c *CategoryService) CreateCategory(ctx context.Context, in *pb.CreateCategoryRequest) (*pb.CreateCategoryResponse, error) {
+	category, err := c.CategoryDB.Create(in.Name, in.Description)
+	if err != nil {
+		return nil, err
+	}
+
+	categoryResult := &pb.Category{
+		Id:          category.ID,
+		Name:        category.Name,
+		Description: category.Description,
+	}
+
+	return &pb.CreateCategoryResponse{Category: categoryResult}, nil
+}
